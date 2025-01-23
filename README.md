9435ae1fa8be084e0d849460543979ef20284c9c
BEYZAASLAN <aslanbeyza3413@gmail.com>
2025-01-23 23:53:42 +0300

    now ı will set password with bcrypt

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ nest g module auth
 nest g controller  auth --no-spec 
 nest g service  auth --no-spec   
 npm run start:dev
+npm i @nestjs/mongoose mongoose./
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
+/* burda kullanıcıyı database kısmına kaydetmek istiyoruz  o zaman burda modelini olusturabilirz */
 export class AuthService {
-  register() {
-    return 'Register page';
-  }
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+  /* Body deki herşeyi al ve dto değişkenşne koy diyoruz burada */
+  async register(dto:AuthDto){ 
+    /* kullanıcnın verdiklerini almak için dto olusturcaz data transfer object */
+    const newUser = new this.userModel({
+      email: dto.email,
+      password: dto.password
+    })
+    return  await newUser.save();
+   }
 }
